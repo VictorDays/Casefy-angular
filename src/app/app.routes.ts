93a7079ca20dcf0376ffff766dc2f1bdc56f1152
Luahr Rocha <73291538+luahr13@users.noreply.github.com>
2024-03-17 21:04:10 -0300

    Lista de mkodelos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ListAdmComponent } from './components/administradores/list/list.component';
 import { FormAdmComponent } from './components/administradores/form/form.component';
+import { ModeloListComponent } from './components/modelo/modelo-list/modelo-list.component';
 
 export const routes: Routes = [
 
     { path: 'adm/list', component: ListAdmComponent, title: 'Administradores'},
     { path: 'adm/form', component: FormAdmComponent, title: 'Cadastro de Administradores'},
+    { path: 'modelos/list', component: ModeloListComponent, title: 'Modelos'},
 ];
